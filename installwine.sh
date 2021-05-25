34d8533ed34a319605cce101f3585e93d0d9e8d9
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-05-25 12:48:29 -0500

    Update installwine.sh

--- installwine.sh
+++ installwine.sh
@@ -2,6 +2,7 @@
 dpkg --add-architecture i386
 apt-get update
 apt-get install -y gnupg software-properties-common unzip zenity
+apt-get autoclean
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | apt-key add - 
 add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 wget -nv https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key -O Release.key
