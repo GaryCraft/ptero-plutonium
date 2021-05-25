3f3921b821efe4ae1ead77267db4d9be31f1cbec
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-05-24 23:46:59 -0500

    Update installwine.sh
    fixed

--- installwine.sh
+++ installwine.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 dpkg --add-architecture i386
+apt-get install --assume-yes software-properties-common
+apt-get update
 wget -qO- https://dl.winehq.org/wine-builds/winehq.key | apt-key add -
 apt-add-repository 'deb http://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-apt-get install --no-install-recommends winehq-stable
+apt-get install --no-install-recommends --assume-yes winehq-stable
