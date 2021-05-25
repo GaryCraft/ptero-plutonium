ed5218a94753aa87926ecbc6372fe3bec7794d48
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-05-25 00:30:54 -0500

    Update installwine.sh

--- installwine.sh
+++ installwine.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 dpkg --add-architecture i386
-echo "Architecture i386 Added"
+echo "------Architecture i386 Added"
 
 apt-get install --assume-yes software-properties-common
 apt-get update
 wget -qO- https://dl.winehq.org/wine-builds/winehq.key | apt-key add -
 apt-add-repository 'deb http://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-echo "WineHQ Repository added"
+echo "------WineHQ Repository added"
 
 wget -qO- https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key | sudo apt-key add -
-sh -c 'echo "deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./" > /etc/apt/sources.list.d/obs.list'
+bash -c 'echo "deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./" > /etc/apt/sources.list.d/obs.list'
 apt update
-echo "Dependencies installed"
+echo "------Dependencies installed"
 
 apt-get install --assume-yes wine-stable-i386 wine-stable-amd64
 apt-get install --assume-yes wine-stable
 apt-get install --no-install-recommends --assume-yes winehq-stable
-echo "WineHQ Installed"
+echo "------WineHQ Installed"
