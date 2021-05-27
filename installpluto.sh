1fe1a5e6cdfbac0234aaf5b6d8e1c13c3f7cc0cc
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-05-26 21:19:35 -0500

    Create installpluto.sh

---
+++ installpluto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get update &&
+wget  https://cdn.plutonium.pw/updater/plutonium.exe &&
+wget  https://github.com/xerxes-at/T6ServerConfigs/archive/master.zip &&
+unzip -n master.zip
