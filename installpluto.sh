134658d051d72e2d29572e88319b0f802d7f6a19
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-05-26 21:50:18 -0500

    Update installpluto.sh

--- installpluto.sh
+++ installpluto.sh
@@ -2,4 +2,8 @@
 apt-get update &&
 wget  https://cdn.plutonium.pw/updater/plutonium.exe &&
 wget  https://github.com/xerxes-at/T6ServerConfigs/archive/master.zip &&
-unzip -n master.zip
+unzip -n master.zip &&
+mv ./T6ServerConfigs-master/* ./* &&
+bash pluto.sh up &&
+rm -r main players sound video &&
+rm 
