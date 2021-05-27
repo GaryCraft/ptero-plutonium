1617bf6bcc122546a3633e9174ed6d41708bded7
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-05-26 23:01:22 -0500

    Update installpluto.sh

--- installpluto.sh
+++ installpluto.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-apt-get update &&
-wget  https://cdn.plutonium.pw/updater/plutonium.exe &&
-wget  https://github.com/xerxes-at/T6ServerConfigs/archive/master.zip &&
-unzip -n master.zip &&
-mv ./T6ServerConfigs-master/* ./* &&
+mkdir /home/container &&
+wget  -O /home/container/plutonium.exe https://cdn.plutonium.pw/updater/plutonium.exe &&
+wget -O /home/container/master.zip https://github.com/xerxes-at/T6ServerConfigs/archive/master.zip &&
+unzip -n master.zip -d /home/container &&
+mv /home/container/T6ServerConfigs-master/* /home/container/* &&
+cd /home/conatiner/ &&
 bash pluto.sh up &&
 rm -r main players sound video &&
 rm codlogo.bmp installscript.vdf localization.txt steam_api.dll Support.url t6mp.exe t6sp.exe t6zm.exe &&
