cfaeee2d87119972eaa2d774f1ab52f51c4c4816
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-05-27 21:49:44 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ cd /home/container
 # Output Current Wine Version
 wine --version /nogui
 rm start.sh
-MF_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ${MF_STARTUP} > start.sh
+
+echo `eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')` >> start.sh
 
 MODIFIED_STARTUP=`bash /home/container/start.sh`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
