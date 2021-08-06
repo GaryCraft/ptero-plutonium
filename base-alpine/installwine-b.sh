ad11cdcbec368565dfa02a2c2a0cf57a5fb25a6c
GaryCraft <garycraft@our-space.xyz>
2021-08-06 11:22:13 -0500

    Update installwine-b.sh

--- base-alpine/installwine-b.sh
+++ base-alpine/installwine-b.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-apk add --no-cache wine=3.0.4-r1 freetype=2.10.0-r0 &&\
+apk add --no-cache wine freetype &&\
 apk update && apk add wine && ln -s /usr/bin/wine64 /usr/bin/wine &&\
 winecfg &&\
-apk add --no-cache wget=1.20.3-r0 &&\
-wget -P /mono http://dl.winehq.org/wine/wine-mono/4.9.3/wine-mono-4.9.3.msi &&\
-wine msiexec /i /mono/wine-mono-4.9.3.msi &&\
-rm -rf /mono/wine-mono-4.9.3.msi &&\
-apk add --no-cache xvfb-run=1.20.4-r0 &&\
+apk add --no-cache wget &&\
+wget -P /mono https://dl.winehq.org/wine/wine-mono/6.3.0/wine-mono-6.3.0-x86.msi &&\
+wine msiexec /i /mono/wine-mono-6.3.0-x86.msi &&\
+rm -rf /mono/wine-mono-6.3.0-x86.msi &&\
+apk add --no-cache xvfb-run &&\
 wine --version &&\
 echo "Wine Installed, Proceeding"
