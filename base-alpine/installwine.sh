3ec1efa1f82da28d0162c8ee28b9bea4569b8fd4
GaryCraft <garycraft@our-space.xyz>
2021-08-05 22:01:27 -0500

    Update installwine.sh

--- base-alpine/installwine.sh
+++ base-alpine/installwine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apk add --no-cache wine=6.0-r1 freetype=2.11.0-r0&&\
+apk add --no-cache wine=6.0-r1 freetype &&\
 winecfg &&\
 wine --version &&\
 sed -i 's/NoDisplay=false/NoDisplay=true/' /usr/share/applications/wine.desktop
