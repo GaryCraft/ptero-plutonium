8761bb92ebd3801a3f43d20cd07a1daa63da027f
GaryCraft <garycraft@our-space.xyz>
2021-08-05 23:49:29 -0500

    Update installwine.sh

--- base-alpine/installwine.sh
+++ base-alpine/installwine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apk add --no-cache wine=6.0-r1 freetype &&\
 apk update && apk add wine && ln -s /usr/bin/wine64 /usr/bin/wine &&\
+winecfg &&\
 wine --version &&\
 echo "Wine Installed, Proceeding"
