d72c692b3c89cd2f510e482193251b2c031739a1
GaryCraft <garycraft@our-space.xyz>
2021-08-06 11:59:11 -0500

    Create entrypoint.sh

---
+++ alternate-alpine/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/container
+
+# Output Current Wine Version
+wine --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
