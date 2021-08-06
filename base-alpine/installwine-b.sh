d50d56edf306d584b7f4772f43019c930ef4899c
GaryCraft <garycraft@our-space.xyz>
2021-08-06 11:28:48 -0500

    Update installwine-b.sh

--- base-alpine/installwine-b.sh
+++ base-alpine/installwine-b.sh
@@ -2,7 +2,6 @@
 apk del wine &&\
 apk add --no-cache wine=6.0-r1 freetype &&\
 apk update && apk add wine && ln -s /usr/bin/wine64 /usr/bin/wine &&\
-winecfg &&\
 apk add --no-cache wget &&\
 wget -P /mono https://dl.winehq.org/wine/wine-mono/6.3.0/wine-mono-6.3.0-x86.msi &&\
 wine msiexec /i /mono/wine-mono-6.3.0-x86.msi &&\
