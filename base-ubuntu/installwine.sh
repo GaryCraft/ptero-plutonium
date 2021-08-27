6ef7eb4096579979b89b1c3dc0a48ae7a9dc3b47
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-08-27 11:33:49 -0500

    install xvfb

--- base-ubuntu/installwine.sh
+++ base-ubuntu/installwine.sh
@@ -9,5 +9,6 @@ apt-key add - < Release.key &&
 apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./' &&
 apt-get update &&
 apt install -y --install-recommends winehq-stable &&
+apt install -y xvfb &&
 wine --version &&
 sed -i 's/NoDisplay=false/NoDisplay=true/' /usr/share/applications/wine.desktop
