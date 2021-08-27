9f2cb33df8c69feecdc7d60744c4d7ce7d85e28d
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-08-27 11:42:20 -0500

    remove noDisplay

--- base-ubuntu/installwine.sh
+++ base-ubuntu/installwine.sh
@@ -2,6 +2,7 @@
 dpkg --add-architecture i386 &&
 apt-get update &&
 apt-get install -y gnupg software-properties-common unzip zenity &&
+apt install -y xvfb &&
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | apt-key add - &&
 add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' &&
 wget -nv https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key -O Release.key &&
@@ -9,6 +10,6 @@ apt-key add - < Release.key &&
 apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./' &&
 apt-get update &&
 apt install -y --install-recommends winehq-stable &&
-apt install -y xvfb &&
 wine --version &&
-sed -i 's/NoDisplay=false/NoDisplay=true/' /usr/share/applications/wine.desktop
+
+#sed -i 's/NoDisplay=false/NoDisplay=true/' /usr/share/applications/wine.desktop
