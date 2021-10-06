d9efb59c0f572bb0e6ea04072ba208f53ed18276
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-10-06 15:23:08 -0500

    Updating to 20.04

--- base-ubuntu/installwine.sh
+++ base-ubuntu/installwine.sh
@@ -1,20 +1,7 @@
-#!/bin/bash
-dpkg --add-architecture i386 &&
-apt-get update &&
-apt-get install -y gnupg software-properties-common unzip zenity &&
-apt install -y xvfb &&
-wget -O - https://dl.winehq.org/wine-builds/winehq.key | apt-key add - &&
-add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main' &&
-wget -nv https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key -O Release.key &&
-apt-key add - < Release.key &&
-apt-add-repository 'deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/ ./' &&
-apt-get update &&
-apt install -y --install-recommends winehq-stable &&
-wine --version && 
-apt-get install -y winetricks &&
-#winetricks dotnet45 &&
-
-wget https://dl.winehq.org/wine/wine-mono/6.0.0/wine-mono-6.0.0-x86.msi -O mono.msi &&
-wine mono.msi &&
-rm mono.msi &&
-echo "Install Finished"
+dpkg --add-architecture i386
+apt update
+wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+apt install software-properties-common
+apt-add-repository "deb http://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
+apt install --install-recommends winehq-stable
+wine --version
