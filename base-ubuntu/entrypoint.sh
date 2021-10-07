e7d037a591606973bdd62692cfc2e9e4d2b79459
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-10-07 17:02:38 -0500

    Start xvfb only on container start

--- base-ubuntu/entrypoint.sh
+++ base-ubuntu/entrypoint.sh
@@ -4,6 +4,9 @@ cd /home/container
 # Output Current Wine Version
 wine --version
 
+# Setup Virtual Screen 
+Xvfb :0 -screen 0 1024x768x16 
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
