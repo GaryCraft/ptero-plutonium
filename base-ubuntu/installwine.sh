0b914d8eaec996d2ac98cfe9a11b394c14195ea1
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-10-07 23:37:44 -0500

    move ENV up

--- base-ubuntu/installwine.sh
+++ base-ubuntu/installwine.sh
@@ -36,7 +36,7 @@ cp winetricks /usr/local/bin
 
 
 # Configure our wine environment
-WINEDEBUG=fixme-all WINEARCH=win64 WINEPREFIX=/home/container/.wine winecfg
+winecfg
 
 # Install Mono
 wget -P /mono http://dl.winehq.org/wine/wine-mono/4.9.4/wine-mono-4.9.4.msi
