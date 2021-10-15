0de8630081a822b7d2b17ff454477383371209e2
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-10-15 10:46:36 -0500

    Try to change working directory from image

--- ptero-plutonium/entrypoint.sh
+++ ptero-plutonium/entrypoint.sh
@@ -10,7 +10,7 @@ wine --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+echo "Running ${MODIFIED_STARTUP}"
 
 # Run the Server
 ( ${MODIFIED_STARTUP} )
