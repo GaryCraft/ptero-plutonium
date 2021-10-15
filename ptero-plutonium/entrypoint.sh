329b8a33f602381b5a1764727e0e063e64fbc155
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-10-15 10:41:36 -0500

    made separate image, execute in subshell

---
+++ ptero-plutonium/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /home/container
+
+# Output Current Wine Version
+wine --version
+
+# Setup Virtual Screen 
+#Xvfb :0 -screen 0 1024x768x16 -nolisten tcp -nolisten unix
+#export DISPLAY=:0.0
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+( ${MODIFIED_STARTUP} )
