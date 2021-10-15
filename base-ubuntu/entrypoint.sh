b513c8539a3be136b1d518f45a8c3bc34304c701
GaryCraft <68017132+GaryCraft@users.noreply.github.com>
2021-10-15 01:29:38 -0500

    cannot issue subshell from entrypoint

--- base-ubuntu/entrypoint.sh
+++ base-ubuntu/entrypoint.sh
@@ -13,4 +13,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-(${MODIFIED_STARTUP})
+${MODIFIED_STARTUP}
