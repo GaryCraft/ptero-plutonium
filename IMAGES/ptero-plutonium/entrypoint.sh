d56a97ef974ca61f392c923619bcb82c0b009bf7
GaryCraft <garycraft001@gmail.com>
2022-03-10 00:25:49 -0600

    add ENV to ptero-plutonium

--- IMAGES/ptero-plutonium/entrypoint.sh
+++ IMAGES/ptero-plutonium/entrypoint.sh
@@ -25,9 +25,11 @@ fi;
 # Xvfb :0 -screen 0 1024x768x16 -nolisten unix
 # export DISPLAY=:0.0
 
+
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo "Running ${MODIFIED_STARTUP}"
 
 # Run the Server
-( cd /home/container/Plutonium && exec xvfb-run wine ${MODIFIED_STARTUP} )
+( cd /home/container/Plutonium && exec WINEDEBUG=fixme-all xvfb-run wine ${MODIFIED_STARTUP} )
