c573dd76455136add3e03af136778d8549dd1231
GaryCraft <garycraft@our-space.xyz>
2024-11-27 14:49:57 -0600

    Structure changes, pluto refactor

--- IMAGES/ptero-plutonium/entrypoint.sh
+++ IMAGES/ptero-plutonium/entrypoint.sh
@@ -2,7 +2,7 @@
 cd /home/container
 
 # Use checkupdater.sh to download the latest updater and run it
-/checkupdater.sh
+# /checkupdater.sh
 
 # Output Current Wine Version
 wine --version
@@ -12,13 +12,6 @@ then echo "Wineprefix not found, initialiizing wine" && /usr/sbin/winetricks
 echo "Configured Succesfully"
 fi;
 
-# Create Shortcuts for zone files
-if [ ! -e /home/container/Server/Zombie/zone ];
-then ln -s /home/container/Server/zone /home/container/Server/Zombie/zone
-fi;
-if [ ! -e /home/container/Server/Multiplayer/zone ];
-then ln -s /home/container/Server/zone /home/container/Server/Multiplayer/zone
-fi;
 
 # Setup Virtual Screen 
 # Xvfb :0 -screen 0 1024x768x16 -nolisten unix
@@ -27,7 +20,11 @@ export WINEDEBUG=fixme-all
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "Running ${MODIFIED_STARTUP}"
+UPDATE_STARTUP=`bin/plutonium-bootstrapper-win32.exe -update-only`
 
+# Run Plutonium as self-updater
+( cd /home/container/Plutonium && exec xvfb-run wine ${UPDATE_STARTUP} )
+
+echo "Running ${MODIFIED_STARTUP}"
 # Run the Server
 ( cd /home/container/Plutonium && exec xvfb-run wine ${MODIFIED_STARTUP} )
