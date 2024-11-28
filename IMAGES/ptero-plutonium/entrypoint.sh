a9be8af34c3ba6a7b4dbe33ca2f2e9ea815a2e91
GaryCraft <garycraft@our-space.xyz>
2024-11-27 18:02:55 -0600

    Try fix path for update command

--- IMAGES/ptero-plutonium/entrypoint.sh
+++ IMAGES/ptero-plutonium/entrypoint.sh
@@ -12,6 +12,8 @@ then echo "Wineprefix not found, initialiizing wine" && /usr/sbin/winetricks
 echo "Configured Succesfully"
 fi;
 
+UPDATE_COMMAND="bin/plutonium-bootstrapper-win32.exe -update-only"
+
 
 # Setup Virtual Screen 
 # Xvfb :0 -screen 0 1024x768x16 -nolisten unix
@@ -20,7 +22,8 @@ export WINEDEBUG=fixme-all
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-UPDATE_STARTUP=`bin/plutonium-bootstrapper-win32.exe -update-only`
+//UPDATE_STARTUP=`bin/plutonium-bootstrapper-win32.exe -update-only`
+UPDATE_STARTUP=`eval echo $(echo ${UPDATE_COMMAND} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 # Run Plutonium as self-updater
 ( cd /home/container/Plutonium && exec xvfb-run wine ${UPDATE_STARTUP} )
